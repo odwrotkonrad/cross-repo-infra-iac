7179b14c9c58ef6c6ef32d8f51e34359ef8bf2ba
konradodwrot <odwrotkonrad@gmail.com>
2026-08-22 19:09:34 +0200

    infra(iac): rename control token, add automation reviewer variable
    gitlab group access token "control-maintainer" renamed to "ko-automation"
    add gitlab_project_variable automation_reviewer, key REPO_VAR_AUTOMATION_REVIEWER, unmasked, set on control project
    add automation_reviewer string variable to control module, auth module, root tf/variables.tf (default "konradodwrot")
    wire automation_reviewer through tf/main.tf -> auth module -> control module

--- tf/modules/auth/control/gitlab.tf
+++ tf/modules/auth/control/gitlab.tf
@@ -1,7 +1,7 @@
 ##[>] 🤖🤖
 resource "gitlab_group_access_token" "control" {
   group        = var.gitlab_group_id
-  name         = "control-maintainer"
+  name         = "ko-automation"
   scopes       = ["api", "write_repository", "read_repository"]
   access_level = "maintainer"
   expires_at   = var.token_expires_at
@@ -13,5 +13,12 @@ resource "gitlab_project_variable" "control_gitlab_token" {
   value     = gitlab_group_access_token.control.token
   masked    = true
   protected = true
+}
+
+resource "gitlab_project_variable" "automation_reviewer" {
+  project = var.control_project_path
+  key     = "REPO_VAR_AUTOMATION_REVIEWER"
+  value   = var.automation_reviewer
+  masked  = false
 }
 ##[<] 🤖🤖
